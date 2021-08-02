2be652d8ace066a78282ea44d291b6f1ca7c8c59
Oleg Popko <goodmans.too@gmail.com>
2021-08-02 13:17:04 +0300

    first commit

---
+++ src/main/java/utils/LoginPage.java
@@ -0,0 +1,9 @@
+package utils;
+//TODO list:
+// - git(gitignore ,2 branches, github)
+// - gradle dependencies(Junit, Selenide)
+// - project structure(classes, features)
+// - what to use(page object, element objects ... etc
+
+public class LoginPage {
+}
