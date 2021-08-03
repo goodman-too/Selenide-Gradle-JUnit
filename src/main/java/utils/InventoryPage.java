517bec5043ca9b3495a86b7ebb4a5951c5b46c7e
Oleg Popko <goodmans.too@gmail.com>
2021-08-03 17:24:10 +0300

    add some selectors

--- src/main/java/utils/InventoryPage.java
+++ src/main/java/utils/InventoryPage.java
@@ -2,13 +2,20 @@ package utils;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 
 public class InventoryPage extends HeaderElements {
 
+    SelenideElement addBackpackButton = $("add-to-cart-sauce-labs-backpack");
+    SelenideElement addBikeLightButton = $("add-to-cart-sauce-labs-bike-light");
+    SelenideElement addBoltTshirtButton = $("add-to-cart-sauce-labs-bolt-t-shirt");
+    SelenideElement addJacketButton = $("add-to-cart-sauce-labs-fleece-jacket");
+    SelenideElement addOnesieButton = $("add-to-cart-sauce-labs-onesie");
+    SelenideElement addRedTshirtButton = $("add-to-cart-test.allthethings()-t-shirt-(red)");
 
     public void addAllItemsToCart() {
-        $$("button.btn_inventory ").forEach(SelenideElement::click);
+        $$("button.btn_inventory").forEach(SelenideElement::click);
     }
 }
